b640dd4aebef58ca00ae441d8a10488047d9ce1b
Nireeksha575 <nireeksha006@gmail.comgit config --global user.email nireeksha006@gmail.comgit config --global user.email nireeksha006@gmail.com>
2026-06-19 10:46:49 +0530

    added tests

---
+++ src/main/java/com/example/Ci_Cd_Pipeline/MockMvcConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.Ci_Cd_Pipeline;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MockMvcConfiguration {
+
+}
